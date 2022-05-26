2ca22cbc6448bd7d9190d526f49b763f8a55e38b
Nafis Shahriar <shaan@Nafiss-MacBook-Air.local>
2022-05-26 15:21:08 +0600

    topic notification removed from web

--- lib/backend/hive_data.dart
+++ lib/backend/hive_data.dart
@@ -1,9 +1,11 @@
+import 'dart:io';
+
 import 'package:Clubzey/backend/dio/club_data.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
+import 'package:flutter/foundation.dart' show kIsWeb;
 class EmailHiveData {
   Future<Box> initHive() async {
     await Hive.initFlutter();
@@ -48,17 +50,21 @@ class TopicSubscription{
   }
 
   calculateSubscription() async{
-    var admin=FirebaseAuth.instance.currentUser!.email;
-    List newSubscriptions= (await ClubData().getAllClubsOnce(admin: admin!)).map((e) => e.getId).toList();
 
-    List oldSubscriptions=await subscriptionMembersList;
 
-     List unSubscriptions=oldSubscriptions.toSet().difference(newSubscriptions.toSet()).toList();
+    if(!kIsWeb){
+      var admin=FirebaseAuth.instance.currentUser!.email;
+      List newSubscriptions= (await ClubData().getAllClubsOnce(admin: admin!)).map((e) => e.getId).toList();
 
-    unSubscriptions.forEach((s) {
-      FirebaseMessaging.instance.unsubscribeFromTopic(s);
-    });
-    updateSubscription(subscriptions: newSubscriptions);
+      List oldSubscriptions=await subscriptionMembersList;
+
+      List unSubscriptions=oldSubscriptions.toSet().difference(newSubscriptions.toSet()).toList();
+
+      unSubscriptions.forEach((s) {
+        FirebaseMessaging.instance.unsubscribeFromTopic(s);
+      });
+      updateSubscription(subscriptions: newSubscriptions);
+    }
 
 
 
@@ -69,12 +75,16 @@ class TopicSubscription{
 
 
   updateSubscription({required List subscriptions}) async {
-    subscriptions.forEach((s) {
-      FirebaseMessaging.instance.subscribeToTopic(s);
-    });
-    Box box = await initHive();
-    print(subscriptions);
-    await box.put("subscriptionMembersList", subscriptions);
+    if(!kIsWeb) {
+      subscriptions.forEach((s) {
+        FirebaseMessaging.instance.subscribeToTopic(s);
+      });
+      Box box = await initHive();
+      print(subscriptions);
+      await box.put("subscriptionMembersList", subscriptions);
+    }
+
+
 
   }
 
--- lib/views/create_invitation_code_page.dart
+++ lib/views/create_invitation_code_page.dart
@@ -5,7 +5,7 @@ import 'package:Clubzey/components/buttons.dart';
 import 'package:Clubzey/utils/allColors.dart';
 import 'package:Clubzey/utils/fontSize.dart';
 import 'package:Clubzey/utils/timer.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
+
 import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
 
